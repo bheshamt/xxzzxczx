4038e0f0c1a09c82808154e53b8d2f3a70732749
bhesham <147453010+bheshamt@users.noreply.github.com>
2024-09-12 10:13:10 -0400

    Create arithmetic.js

---
+++ arithmetic.js
@@ -0,0 +1,23 @@
+function PerformCalc()
+{
+	return (2+3)
+}
+document.write("The sum of 2 + 3 = " + PerformCalc() + ".\n")
+
+function PerformSub()
+{
+	return (10-3)
+}
+document.write("The difference of 10 - 3 = " + PerformSub() + ".\n")
+
+function PerformMulti()
+{
+	return(5*4)
+}
+document.write("The product of 5 x 4 = " + PerformMulti() + ".\n")
+
+function PerformDiv()
+{
+	return(64/4)
+}
+document.write("The quotient of 64 / 3 = " + PerformDiv() + ".\n")

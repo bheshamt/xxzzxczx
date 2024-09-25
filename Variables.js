b2ec4c024bd22f125da43d2b30aef94016bf5188
bhesham <147453010+bheshamt@users.noreply.github.com>
2024-09-25 10:22:53 -0400

    Update Variables.js

--- Variables.js
+++ Variables.js
@@ -1,13 +1,20 @@
 window.addEventListener("load",addListener);
-var sum = "";
+var num1, num2, res;
+res = ""
 function addListener()
 {
-	document.getElementById("btnone").addEventListener("click",ButtonOne);
+	document.getElementById("txtres").disabled = true;
+	document.getElementById("btnadd").addEventListener("click",Addition);
+	document.getElementById("btnsub").addEventListener("click",Subtraction);
+	document.getElementById("btnmult").addEventListener("click",Multiplication);
+	document.getElementById("btndiv").addEventListener("click",Division);
+	document.getElementById("btnclear").addEventListener("click",Clear);
 }
 
-function ButtonOne()
+function Addition()
 {
-	var numone = document.getElementById("txtnumone").value;
-	sum = numone + numone;
-	document.getElementById("lblanswer").textContent = sum;
+	var num1 = document.getElementById("txtnum1").value
+	var num2 = document.getElemntById("txtnum2").value
+	res = parseInt(num1) + parseint(num2);
+	document.getElementById("txtres").textContent = res;
 }

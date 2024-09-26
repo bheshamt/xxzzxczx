d62f9482eecd6303df3c2cc805cfced8a178e191
bhesham <147453010+bheshamt@users.noreply.github.com>
2024-09-25 20:28:25 -0400

    Update Variables.js

--- Variables.js
+++ Variables.js
@@ -1,20 +1,24 @@
 window.addEventListener("load",addListener);
-var num1, num2, res;
-res = ""
+var ans = ""
 function addListener()
 {
 	document.getElementById("txtres").disabled = true;
 	document.getElementById("btnadd").addEventListener("click",Addition);
 	document.getElementById("btnsub").addEventListener("click",Subtraction);
-	document.getElementById("btnmult").addEventListener("click",Multiplication);
-	document.getElementById("btndiv").addEventListener("click",Division);
-	document.getElementById("btnclear").addEventListener("click",Clear);
 }
 
 function Addition()
 {
-	var num1 = document.getElementById("txtnum1").value
-	var num2 = document.getElemntById("txtnum2").value
-	res = parseInt(num1) + parseint(num2);
-	document.getElementById("txtres").textContent = res;
+	var numone = document.getElementById("txtnum1").value;
+	var numtwo = document.getElementById("txtnum2").value;
+	ans = parseInt(numone) + parseInt(numtwo);
+	document.getElementById("txtres").textContent = ans;
+}
+
+function Subtraction()
+{
+	var numone = document.getElementById("txtnum1").value;
+		var numtwo = document.getElementById("txtnum2").value;
+		ans = parseInt(numone) - parseInt(numtwo);
+		document.getElementById("txtres").textContent = ans;
 }

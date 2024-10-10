ec81039fbfc0b50f38a599d359863bf2a591e7df
bhesham <147453010+bheshamt@users.noreply.github.com>
2024-10-10 10:20:50 -0400

    Update gddf.js

--- gddf.js
+++ gddf.js
@@ -1,137 +1,39 @@
 window.addEventListener("load",addListener);
-var input, tempnum, tempop, res
-tempop = ""
-tempnum = ""
+
 function addListener()
 {
-	document.getElementById("txtnumdisplay").disabled = true;
-	document.getElementById("btnclear").addEventListener("click",clear)
-	document.getElementById("btn7").addEventListener("click",seven)
-	document.getElementById("btn8").addEventListener("click",eight)
-	document.getElementById("btn9").addEventListener("click",nine)
-	document.getElementById("btn4").addEventListener("click",four)
-	document.getElementById("btn5").addEventListener("click",five)
-	document.getElementById("btn6").addEventListener("click",six)
-	document.getElementById("btn1").addEventListener("click",one)
-	document.getElementById("btn2").addEventListener("click",two)
-	document.getElementById("btn3").addEventListener("click",three)
-	document.getElementById("btn0").addEventListener("click",zero)
-	document.getElementById("btndecimal").addEventListener("click",decimal)
-	document.getElementById("btnadd").addEventListener("click",add)
-	document.getElementById("btnsubtract").addEventListener("click",subtract)
-	document.getElementById("btnmultiply").addEventListener("click",multiply)
-	document.getElementById("btndivide").addEventListener("click",divide)
+	document.getElementById("btngrade").addEventListener("click",CheckGrade);
 }
 
-function add()
+function CheckGrade()
 {
-    tempop = "+";
-    document.getElementById("txtnumdisplay").value = "";
-}
-
-function subtract()
-{
-	document.getElementById("txtnumdisplay").value = "";
-    tempop = "-";
-	enter
-}
-
-function multiply()
-{
-    tempop = "x";
-    document.getElementById("txtnumdisplay").value = "";
-	enter
-}
-
-function divide()
-{
-    tempop = "/";
-    document.getElementById("txtnumdisplay").value = "";
-	enter
-}
-
-function enter()
-{
-	switch (tempop)
+	var grade, msg = "Please enter the proper grade from 0-100";
+	
+	grade = document.getElementById("txtgrade").value;
+	
+	if (grade == "" || (grade < 0 || grade > 100))
 	{
-		case "+":
-			tempnum = document.getElementById("txtnumdisplay").value;
-            res = parseFloat(input) + parseFloat(tempnum);
-			document.getElementById("txtnumdisplay").value = res;
-        case "-":
-			tempnum = document.getElementById("txtnumdisplay").value;
-            res = parseFloat(tempnum) - parseFloat(input);
-			document.getElementById("txtnumdisplay").value = res;
-        case "x":
-			tempnum = document.getElementById("txtnumdisplay").value;
-            res = parseFloat(tempnum) * parseFloat(input);
-			document.getElementById("txtnumdisplay").value = res;
-        case "/":
-			tempnum = document.getElementById("txtnumdisplay").value;
-            res = parseFloat(tempnum) / parseFloat(input);
-			document.getElementById("txtnumdisplay").value = res;
-		case "":
-			alert("Please enter something.")
-			break;
+		alert(msg);
+		document.getElementById("txtgrade").value = "";
+	}
+	else
+	{
+		GPAGrade(checkgpa);
 	}
 }
 
-function clear()
+function GPAGrade(gpa)
 {
-	document.getElementById("txtnumdisplay").value = null;
-}
-
-function seven()
-{
-    input = document.getElementById("txtnumdisplay").value += "7";
-}
-
-function eight()
-{
-    input = document.getElementById("txtnumdisplay").value += "8";
-}
-
-function nine()
-{
-    input = document.getElementById("txtnumdisplay").value += "9";
-}
-
-function four()
-{
-    input = document.getElementById("txtnumdisplay").value += "4";
-}
-
-function five()
-{
-    input = document.getElementById("txtnumdisplay").value += "5";
-}
-
-function six()
-{
-    input = document.getElementById("txtnumdisplay").value = "6";
-}
-
-function one()
-{
-    input = document.getElementById("txtnumdisplay").value += "1";
-}
-
-function two()
-{
-    input = document.getElementById("txtnumdisplay").value += "2";
-}
-
-function three()
-{
-    input = document.getElementById("txtnumdisplay").value += "3";
-}
-
-function zero()
-{
-    input = document.getElementById("txtnumdisplay").value += "0";
-}
-
-function decimal()
-{
-	document.getElementById("txtnumdisplay").value += ".";
+	switch(gpa)
+	{
+		case "66":
+			msgdisp = "66";
+			break;
+			
+		default:
+			msgdisp = "Not a valid grade.";
+			break;
+	}
+	
+	document.getElementById("lblgrade").textContent = msgdisp;
 }

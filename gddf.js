188a3f63b8520df9930d86f68bc6564b17348024
bhesham <147453010+bheshamt@users.noreply.github.com>
2024-11-07 10:22:36 -0500

    Update gddf.js

--- gddf.js
+++ gddf.js
@@ -1,62 +1,28 @@
-window.addEventListener("load", addListener);
+window.addEventListener("load",addListener);
+window.addEventListener("load",Init);
 
-let totalGrades = 0;
-let numGrades = 0; 
+function Init()
+{
+	document.getElementById("askclass").style.display = "none"
+}
 
 function addListener()
 {
-    document.getElementById("btnsubmit").addEventListener("click", Loop);
+	document.getElementById("btnsubmit").addEventListener("click",Local);
 }
 
-function Loop()
+function Local()
 {
-    howMany = parseFloat(document.getElementById("txtinput").value);
-    
-    document.getElementById("lbldisp").textContent = "";
-
-    totalGrades = 0;
-    numGrades = 0;
-
-    ForLoop(howMany);
-}
-
-function ForLoop(howMany)
-{
-    for (let i = 0; i < howMany; i++)
-    {
-        let grade = parseFloat(prompt("Enter grade: "));
-
-        totalGrades += grade;
-        numGrades++;
-    }
-    Average();
-}
-
-function Average()
-{
-    let avg = totalGrades / numGrades;
-    gpa = convertToGPA(avg);
-
-    document.getElementById("lbldisp").textContent = "\n Average Grade: " + avg + "\n GPA: " + gpa;
-}
-
-function convertToGPA(avg)
-{
-    switch(avg)
-    {
-        case avg >= 90:
-            return "4.0";
-        
-        case avg >= 80:
-            return "3.0";
-    
-        case avg >= 70:
-            return "2.0";
-
-        case avg >= 60:
-            return "1.0";
-        
-        default:
-            return "0.0";
-    }
+	localStorage.clear();
+	fname = document.getElementById("txtfirstname").value;
+	localStorage.setItem("firstname",fname);
+	lname = document.getElementById("txtlastname").value;
+	localStorage.setItem("lastname",lname);
+	id = document.getElementById("txtid").value;
+	localStorage.setItem("studentid",id);
+	glevel = document.getElementById("txtgradelvl").value;
+	localStorage.setItem("gradelevel",glevel);
+	
+	document.getElementById("askinfo").style.display = "none";
+	document.getElementById("askclass").style.display = "block"
 }

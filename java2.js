697b79be7ad5055366543c2807df85201acf38c1
bhesham <147453010+bheshamt@users.noreply.github.com>
2024-11-12 10:21:23 -0500

    Update java2.js

--- java2.js
+++ java2.js
@@ -8,6 +8,23 @@ function GetInfo()
     gradelevel = localStorage.getItem("gradelevel");
     officialclass = localStorage.getItem("officialclass");
     counselor = localStorage.getItem("counselor");
+	
+	firstclass = localStorage.getItem("firstclass");
+	firstteacher = localStorage.getItem("firstteacher");
+	secondclass = localStorage.getItem("secondclass");
+	secondteacher = localStorage.getItem("secondteacher");
+	thirdclass = localStorage.getItem("thirdclass");
+	thirdteacher = localStorage.getItem("thirdteacher");
+	fourthclass = localStorage.getItem("fourthclass");
+	fourthteacher = localStorage.getItem("fourthteacher");
+	fifthclass = localStorage.getItem("fifthclass");
+	fifthteacher = localStorage.getItem("fifthteacher");
+	sixthclass = localStorage.getItem("sixthclass");
+	sixthteacher = localStorage.getItem("sixthteacher");
+	seventhclass = localStorage.getItem("seventhclass");
+	seventhteacher = localStorage.getItem("seventhteacher");
+	eighthclass = localStorage.getItem("eighthclass");
+	eighthteacher = localStorage.getItem("eighthteacher");
 
     document.getElementById("lastn").textContent = firstname;
     document.getElementById("firstn").textContent = lastname;
